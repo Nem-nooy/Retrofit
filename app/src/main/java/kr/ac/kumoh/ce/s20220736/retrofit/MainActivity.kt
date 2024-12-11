dafef4f215d70fb8099528bd64decb016af4f19b
Nem-nooy <sgu101310@gmail.com>
2024-12-11 17:30:34 +0900

    🚧 SongList 완성

--- app/src/main/java/kr/ac/kumoh/ce/s20220736/retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220736/retrofit/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
@@ -18,6 +22,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import kr.ac.kumoh.ce.s20220736.retrofit.ui.theme.RetrofitTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,16 +52,41 @@ fun MainScreen(viewModel: SongViewModel = viewModel()) {
 @Composable
 fun SongList(list: List<Song>, modifier: Modifier) {
     LazyColumn(
-        modifier,
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 8.dp),
     ) {
 //        item {
 //            Text("Header")
 //        }
         items(list) { song ->
-            Text(song.toString())
+            SongItem(song)
         }
 //        item {
 //            Text("Footer")
 //        }
     }
+}
+
+@Composable
+fun SongItem(song: Song) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xffffffcc))      // 밝은 노란색
+            .padding(16.dp)
+    ) {
+        TextTitle(song.title)
+        TextSinger(song.singer)
+    }
+}
+
+@Composable
+fun TextTitle(title: String) {
+    Text(title, fontSize = 30.sp)
+}
+
+@Composable
+fun TextSinger(singer: String) {
+    Text(singer, fontSize = 20.sp)
 }
