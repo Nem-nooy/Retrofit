2f62fc034248ec47413f87703341084f7ce50291
Nem-nooy <sgu101310@gmail.com>
2024-12-13 17:19:21 +0900

    🍱 AsyncImage 사용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,12 @@ dependencies {
     // NOTE: observeAsState 사용하기 위해 추가
     implementation("androidx.compose.runtime:runtime-livedata:1.7.5")
 
+    // NOTE: AsyncImage 사용하기 위해 추가
+    //implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    //implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/kr/ac/kumoh/ce/s20220736/retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20220736/retrofit/MainActivity.kt
@@ -7,10 +7,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,9 +28,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import kr.ac.kumoh.ce.s20220736.retrofit.ui.theme.RetrofitTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,20 +81,44 @@ fun SongList(list: List<Song>, modifier: Modifier) {
 
 @Composable
 fun SongItem(song: Song) {
-    Column(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color(0xffffffcc))      // 밝은 노란색
-            .padding(16.dp)
+            .height(IntrinsicSize.Min)
+            .background(Color(255, 210, 210))
+            .padding(8.dp)
     ) {
-        TextTitle(song.title)
-        TextSinger(song.singer)
+        AsyncImage(
+            model = "https://picsum.photos/300/300?random=${song.singer}",
+            contentDescription = "가수 이미지 ${song.singer}",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(percent = 10)),
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Column(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .background(Color(0xffffffcc))      // 밝은 노란색
+//                .padding(16.dp)
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            TextTitle(song.title)
+            TextSinger(song.singer)
+        }
     }
 }
 
 @Composable
 fun TextTitle(title: String) {
-    Text(title, fontSize = 30.sp)
+    Text(
+        title,
+        fontSize = 30.sp,
+        lineHeight = 35.sp
+    )
 }
 
 @Composable
